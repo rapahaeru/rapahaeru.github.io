bbe85001a749486763673ff9e80ed6ddb8593e17
Raphael Oliveira <login+rapahaeru@elo7.com>
2018-06-27 12:04:41 -0300

    e-mails lidos

--- public/app/views/HelloView.js
+++ public/app/views/HelloView.js
@@ -2,9 +2,19 @@ class HelloView {
 
 	checkSelectedChoice(elements, choice) {
 		if (choice == "all") {
-			elements.forEach(keys => keys.checked = true);
+			elements.forEach(key => key.checked = true);
 		} else if (choice == "none") {
-			elements.forEach(keys => keys.checked = false);
+			elements.forEach(key => key.checked = false);
+		} else if (choice == "all-readed") {
+			[].map.call(elements, key => {
+				let item = key.parentElement.className;
+
+				if (item.includes("readed")) {
+					key.checked = true;
+				} else {
+					key.checked = false;
+				}
+			});
 		}
 	}
 }
